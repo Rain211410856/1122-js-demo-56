ca37e030a72ef3ba9d084c65973399d7661f8da0
Rain211410856 <211410856@o365.tku.edu.com.tw>
2024-03-12 12:43:00 +0800

    W01-P6: git logs for W01

--- demo/w01_dom_56/js/w01_56.js
+++ demo/w01_dom_56/js/w01_56.js
@@ -16,21 +16,58 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function getUserInput() {
-    return parseInt(userInput.value);
+  return parseInt(userInput.value);
 }
 
 function OutputResult(result, text) {
-    currentCalculationOutput.textContent = text;
-    currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent = text;
+  currentResultOutput.textContent = result;
 }
 
 function add() {
-    const operand1 = currentResult;
-    const operand2 = getUserInput();
-    // console.log(operand1,operand2, operand1 + operand2);
-    currentResult = operand1 + operand2;
-    const calText = `${operand1} + ${operand2}`;
-    OutputResult(currentResult, calText);
+  const operand1 = currentResult;
+  const operand2 = getUserInput();
+  //   console.log(operand1, operand2, operand1 + operand2);
+  currentResult = operand1 + operand2;
+  const calText = `${operand1} + ${operand2}`;
+  console.log(`${operand1} + ${operand2} = ${currentResult}`);
+  OutputResult(currentResult, calText);
 }
 
-addBtn.addEventListener('click' , add);
+addBtn.addEventListener('click', add);
+
+function subtract() {
+  const operand1 = currentResult;
+  const operand2 = getUserInput();
+  currentResult = operand1 - operand2;
+  const calText = `${operand1} - ${operand2}`;
+  console.log(`${operand1} - ${operand2} = ${currentResult}`);
+  OutputResult(currentResult, calText);
+}
+
+subtractBtn.addEventListener('click', subtract);
+
+function multiply() {
+  const operand1 = currentResult;
+  const operand2 = getUserInput();
+  currentResult = operand1 * operand2;
+  const calText = `${operand1} * ${operand2}`;
+  console.log(`${operand1} * ${operand2} = ${currentResult}`);
+  OutputResult(currentResult, calText);
+}
+
+multiplyBtn.addEventListener('click', multiply);
+
+function divide() {
+  const operand1 = currentResult;
+  const operand2 = getUserInput();
+  if (operand2 === 0) alert('cannot divide by 0');
+  else {
+    currentResult = operand1 / operand2;    
+    console.log(`${operand1} / ${operand2} = ${currentResult}`);
+    const calText = `${operand1} / ${operand2}`;
+    OutputResult(currentResult, calText);
+  }
+}
+
+divideBtn.addEventListener('click', divide);
