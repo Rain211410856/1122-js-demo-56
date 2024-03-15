15afbf0562f28290ee958014bf328ada4ea22c8d
Rain211410856 <211410856@o365.tku.edu.com.tw>
2024-03-15 14:12:50 +0800

    W03-P4: Compute highest and lowest score of sdata and students2

--- demo/w03_basics_56/p3_56/p3_56.js
+++ demo/w03_basics_56/p3_56/p3_56.js
@@ -1,6 +1,28 @@
 // 此處為匯入P3_56的兩筆資料
 // JS匯出務必加上檔名.js其他不用
-import { sdata, students} from './data_56.js';
+import { sdata, students } from './data_56.js';
 
 console.log('sdata', sdata);
 console.log('students', students);
+
+sdata.sort();
+console.log('sdata after sorting alphabetically', sdata);
+
+sdata.sort(function (a, b) {
+  return b - a;
+});
+console.log('sdata after sorting descending', sdata);
+console.log(`the highest score: ${sdata[0]}`);
+console.log(`the lowest score: ${sdata[sdata.length - 1]}`);
+
+console.log('students', students);
+const students2 = students.map((student) => {
+  return { ...student, role: 'student' };
+});
+
+students2.sort(function (a, b) {
+  return b.score - a.score;
+});
+console.log('students2 sorted descending', students2);
+console.log(`the highest score: ${students2[0].score}`);
+console.log(`the lowest score:${students2[students2.length - 1].score}`);
