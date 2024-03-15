c32620fbf1f52269a03f694e36481f89a52e0e70
Rain211410856 <211410856@o365.tku.edu.com.tw>
2024-03-15 12:51:52 +0800

    W03-P3: import sdata and students data from data_xx.js

---
+++ demo/w03_basics_56/p3_56/data_56.js
@@ -0,0 +1,32 @@
+// 此範例要匯出兩筆資料sdata&students
+
+// export匯出
+export const sdata = [90,100,60,40,20,80];
+// 物件需要用大括弧隔開
+export const students = [
+    {
+        id: 1,
+        name: 'peter',
+        score: 80,
+    },
+    {
+        id: 2,
+        name: 'jonh',
+        score: 90,
+    },
+    {
+        id: 3,
+        name: 'marry',
+        score: 50,
+    },
+    {
+        id: 4,
+        name: 'Amy',
+        score: 30,
+    },
+    {
+        id: 1,
+        name: 'Lucy',
+        score: 100,
+    },
+];
---
+++ demo/w03_basics_56/p3_56/p3_56.js
@@ -0,0 +1,6 @@
+// 此處為匯入P3_56的兩筆資料
+// JS匯出務必加上檔名.js其他不用
+import { sdata, students} from './data_56.js';
+
+console.log('sdata', sdata);
+console.log('students', students);
