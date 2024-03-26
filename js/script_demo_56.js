870fc1d882520df2b4f21cd98438f3e4a3df58e6
Rain211410856 <211410856@o365.tku.edu.com.tw>
2024-03-26 12:38:29 +0800

    W05-P1: Use megamenu to show class demo w1~W4 locally

---
+++ js/script_demo_56.js
@@ -0,0 +1,30 @@
+function showdemo(week) {
+  const p = document.querySelector('.show-classdemo');
+  console.log('p', p);
+  switch (week) {
+    case 'w1':
+      p.innerHTML = `<iframe src="./demo/w01_dom_56/index.html" width="100%" height="100%" />`;
+      break;
+    case 'w2':
+      p.innerHTML = `<iframe src="./demo/w02_tictactoe_56/tictactoe_56.html" width="100%" height="100%" />`;
+      break;
+    case 'w3-p1':
+      p.innerHTML = `<iframe src="./demo/w03_basics_56/p1_56/p1_56.html" width="100%" height="100%" />`;
+      break;
+    case 'w3-p2':
+      p.innerHTML = `<iframe src="./demo/w03_basics_56/P2_56/P2_56.html" width="100%" height="100%" />`;
+      break;
+    case 'w3-p3':
+      p.innerHTML = `<iframe src="./demo/w03_basics_56/p3_56/p3_56.html" width="100%" height="100%" />`;
+      break;
+    case 'w4-p4':
+      p.innerHTML = `<iframe src="./demo/w04_basics_56/p4_56/p4_56.html" width="100%" height="100%" />`;
+      break;
+    case 'w4-p5':
+      p.innerHTML = `<iframe src="./demo/w04_basics_56/p5_56/p5_56.html" width="100%" height="100%" />`;
+      break;
+    case 'w4-p6':
+      p.innerHTML = `<iframe src="./demo/w04_basics_56/p6_56/P6_56.html" width="100%" height="100%" />`;
+      break;
+  }
+}
