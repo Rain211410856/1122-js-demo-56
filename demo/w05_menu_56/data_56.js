7c247efe71cb9d9ed1836322ab9cb9632dcff299
Rain211410856 <211410856@o365.tku.edu.com.tw>
2024-03-29 18:56:40 +0800

    W06-P1: Create categories dynamically from data_xx.js

--- demo/w05_menu_56/data_56.js
+++ demo/w05_menu_56/data_56.js
@@ -3,6 +3,7 @@ const menu = [
     id: 1,
     title: 'buttermilk pancakes',
     category: 'breakfast',
+    categories:'all',
     price: '15.99',
     img: `./images/item-1.jpeg`,
     desc: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-presseds",
@@ -11,6 +12,7 @@ const menu = [
     id: 2,
     title: 'diner double',
     category: 'lunch',
+    categories:'breakfast',
     price: '13.99',
     img: `./images/item-2.jpeg`,
     desc: 'vaporware iPhone mumblecore selvage raw denim slow-carb leggingsgochujang helvetica man braid jianbing. Marfa thundercats',
@@ -19,6 +21,7 @@ const menu = [
     id: 3,
     title: 'godzilla milkshake',
     category: 'dinner',
+    categories:'lunch',
     price: '6.99',
     img: `./images/item-3.jpeg`,
     desc: 'ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.',
@@ -27,10 +30,12 @@ const menu = [
     id: 4,
     title: 'country delight',
     category: 'breakfast',
+    categories:'shakes',
     price: '20.99',
     img: `./images/item-4.jpeg`,
     desc: 'Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut',
   },
 ];
+
 // 匯入資料只有一筆可以用export，但不能改import，多筆用上面的陣列
 export default menu;
--- demo/w05_menu_56/script_56.js
+++ demo/w05_menu_56/script_56.js
@@ -1,9 +1,9 @@
 import menu from './data_56.js';
 
 const sectionCenter = document.querySelector('.section-center');
-
 //完成後除錯必備 console.log('xxx',xxx);
-console.log('menu',menu);
+console.log('menu', menu);
+
 
 const displayMenuItems = (menu) => {
     let displayMenu = menu.map((item) => {
@@ -26,6 +26,9 @@ const displayMenuItems = (menu) => {
             </p>
           </div>
         </article>
+        <div class="btn-container">
+        <button type="button" class="filter-btn" data-id="all">${title}</button
+      </div>
         `;
     })
     // .join('')之後return這個陣列會變成字串
@@ -34,7 +37,25 @@ const displayMenuItems = (menu) => {
     sectionCenter.innerHTML = displayMenu;
 };
 
-// JS只有一個執行續，會一直往下讀不等人，所以需要callback function的動作，指在執行完通知我
+// const categories = ['all', 'breakfast', 'lunch', 'dinner', 'shakes'];
+// set 動態陣列寫法，可以直接加入別的陣列資料，不用寫一大串
+const menuCategories = new Set(
+  menu.map((item) => {
+    return item.category;
+  })
+);
+console.log('menuCategories', menuCategories);
+
+// 此處...menuCategories = ...某變數的所有資料
+const categories = ['all', ...menuCategories];
+console.log('categories', categories);
+
+const displayMenuButtons = () => {};
+
+// JS只有一個執行續，會一直往下讀不等人，所以需要callback function的動作，
+// 指在執行完通知我，也就是return那一行的作用
+
 window.addEventListener('DOMContentLoaded', () => {
     displayMenuItems(menu);
+    displayMenuButtons();
 });
