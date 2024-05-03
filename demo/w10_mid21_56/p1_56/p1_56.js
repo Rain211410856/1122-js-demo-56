2e8aad8a6a10895f02203aee7d9eea63c18e7cce
Rain211410856 <211410856@o365.tku.edu.com.tw>
2024-05-03 14:25:17 +0800

    W10-P1: In Supabase, create product_xx table with 8 product data

---
+++ demo/w10_mid21_56/p1_56/p1_56.js
@@ -0,0 +1,11 @@
+import { products_56, all_products_56 } from './p1_data_56.js';
+
+const productContainer = document.querySelector('.products-container');
+
+console.log('products_56', products_56);
+
+const DisplayProducts = (products) => {};
+
+document.addEventListener('DOMContentLoaded', () => {
+  DisplayProducts(products_56);
+});
